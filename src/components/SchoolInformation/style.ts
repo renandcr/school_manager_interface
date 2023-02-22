95a05fa014499cdbb45cab7b580d6691e42e7c65
renandcr <renandamagazine@gmail.com>
2023-02-22 18:17:13 -0300

    feat: Desenvolver componente SchoolPage.

--- src/components/SchoolInformation/style.ts
+++ src/components/SchoolInformation/style.ts
@@ -1,6 +1,12 @@
+import { ISchoolInformation } from ".";
 import styled from "styled-components";
 
-export const SchoolInformationContainer = styled.ul`
-  max-width: 100%;
-  /* border: solid 1px; */
+export const SchoolInformationContainer = styled.ul<ISchoolInformation>`
+  width: fit-content;
+  transition: all ease-in 500ms;
+  cursor: ${(props) => props.editable && "pointer"};
+  :hover {
+    transition: all ease-in 300ms;
+    opacity: ${(props) => props.editable && "50%"};
+  }
 `;
--- src/pages/SchoolPage/index.tsx
+++ src/pages/SchoolPage/index.tsx
@@ -1,14 +1,41 @@
 import { MainSchoolPageContainer, SchoolPageContainer } from "./style";
+import { IDatabaseSchool } from "../../components/SchoolInformation";
+import SchoolInformation from "../../components/SchoolInformation";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import api from "../../assets/axios";
 import * as React from "react";
 
 const SchoolPage = () => {
+  const [school, setSchool] = React.useState<IDatabaseSchool[]>();
+
+  React.useEffect(() => {
+    api
+      .get(`/school`, {
+        headers: {
+          Authorization: `Bearer ${"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjc3MTcxOTA4LCJpYXQiOjE2NzcwODU1MDgsImp0aSI6Ijc5MWQyNTc2YjgzYjRjODc4MzhhMmYzOGRhZjdkNDhlIiwidXNlcl9pZCI6IjIyMmQ1NmJlLTE0NzItNDQzMC05ZTM1LTU3NGMwZDUyOTVlYyJ9.cASApv7dizkSNcXO0XNVV4lLzw6bchnUaKwD7GBOLJY"}`,
+        },
+      })
+      .then((response) => {
+        setSchool(response.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
   return (
     <>
       <Header />
       <MainSchoolPageContainer>
-        <SchoolPageContainer></SchoolPageContainer>
+        <SchoolPageContainer>
+          {school &&
+            school.map((current) => (
+              <SchoolInformation
+                key={current.id}
+                name={current.name}
+                email={current.email}
+                editable
+              />
+            ))}
+        </SchoolPageContainer>
       </MainSchoolPageContainer>
       <Footer />
     </>
