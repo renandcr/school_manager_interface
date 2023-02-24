d002e13d5128cf5adc6bd9f2dd6bf2fcca9002c0
renandcr <renandamagazine@gmail.com>
2023-02-24 20:31:52 -0300

    fix: Editar componente HomePage. Alterar layout da página, agora é possível adicionar novas escolas, visualizar todas as escolas e editar escolas.

--- src/components/Modals/OptionsModal/index.tsx
+++ src/components/Modals/OptionsModal/index.tsx
@@ -1,77 +1,78 @@
-import { CloseModalContainer } from "../DefaultModal/style";
-import { VARIABLES } from "../../../styles/global";
-import CloseIcon from "@mui/icons-material/Close";
-import { OptionsModalComponent } from "./style";
-import DefaultButton from "../../DefaultButton";
-import { useHistory } from "react-router-dom";
-import DefaultModal from "../DefaultModal";
-import * as React from "react";
+// import { CloseModalContainer } from "../DefaultModal/style";
+// import { VARIABLES } from "../../../styles/global";
+// import CloseIcon from "@mui/icons-material/Close";
+// import { OptionsModalComponent } from "./style";
+// import DefaultButton from "../../DefaultButton";
+// import { useHistory } from "react-router-dom";
+// import DefaultModal from "../DefaultModal";
+// import * as React from "react";
 
-interface IOptionsModal {
-  showOptionsModal: boolean;
-  setShowOptionsModal: React.Dispatch<boolean>;
-}
+// interface IOptionsModal {
+//   showOptionsModal: boolean;
+//   setShowOptionsModal: React.Dispatch<boolean>;
+// }
 
-const OptionsModal: React.FC<IOptionsModal> = ({
-  showOptionsModal,
-  setShowOptionsModal,
-}) => {
-  const history = useHistory();
+// const OptionsModal: React.FC<IOptionsModal> = ({
+//   showOptionsModal,
+//   setShowOptionsModal,
+// }) => {
+//   const history = useHistory();
 
-  return (
-    <>
-      {showOptionsModal && (
-        <DefaultModal backgroundColor="transparent">
-          <OptionsModalComponent>
-            <CloseModalContainer>
-              <div></div>
-              <CloseIcon
-                className="icon_close"
-                onClick={() => setShowOptionsModal?.(false)}
-              />
-            </CloseModalContainer>
-            <ul>
-              <li onClick={() => history.push("/school_page")}>
-                <DefaultButton
-                  backgroundColor={VARIABLES.blueColor5}
-                  color={VARIABLES.grayColor4}
-                  border={"solid 1px"}
-                  width="180px"
-                  height="55px"
-                >
-                  {"Escola"}
-                </DefaultButton>
-              </li>
+//   return (
+//     <>
+//       {showOptionsModal && (
+//         <DefaultModal backgroundColor="transparent">
+//           <OptionsModalComponent>
+//             <CloseModalContainer>
+//               <div></div>
+//               <CloseIcon
+//                 className="icon_close"
+//                 onClick={() => setShowOptionsModal?.(false)}
+//               />
+//             </CloseModalContainer>
+//             <ul>
+//               <li onClick={() => history.push("/school_page")}>
+//                 <DefaultButton
+//                   backgroundColor={VARIABLES.blueColor5}
+//                   color={VARIABLES.grayColor4}
+//                   border={"solid 1px"}
+//                   width="180px"
+//                   height="55px"
+//                 >
+//                   {"Escola"}
+//                 </DefaultButton>
+//               </li>
 
-              <li>
-                <DefaultButton
-                  backgroundColor={VARIABLES.blueColor5}
-                  color={VARIABLES.grayColor4}
-                  border={"solid 1px"}
-                  width="180px"
-                  height="55px"
-                >
-                  {"Cursos"}
-                </DefaultButton>
-              </li>
+//               <li>
+//                 <DefaultButton
+//                   backgroundColor={VARIABLES.blueColor5}
+//                   color={VARIABLES.grayColor4}
+//                   border={"solid 1px"}
+//                   width="180px"
+//                   height="55px"
+//                 >
+//                   {"Cursos"}
+//                 </DefaultButton>
+//               </li>
 
-              <li>
-                <DefaultButton
-                  backgroundColor={VARIABLES.blueColor5}
-                  color={VARIABLES.grayColor4}
-                  border={"solid 1px"}
-                  width="180px"
-                  height="55px"
-                >
-                  {"Alunos"}
-                </DefaultButton>
-              </li>
-            </ul>
-          </OptionsModalComponent>
-        </DefaultModal>
-      )}
-    </>
-  );
-};
+//               <li>
+//                 <DefaultButton
+//                   backgroundColor={VARIABLES.blueColor5}
+//                   color={VARIABLES.grayColor4}
+//                   border={"solid 1px"}
+//                   width="180px"
+//                   height="55px"
+//                 >
+//                   {"Alunos"}
+//                 </DefaultButton>
+//               </li>
+//             </ul>
+//           </OptionsModalComponent>
+//         </DefaultModal>
+//       )}
+//     </>
+//   );
+// };
 
-export default OptionsModal;
+// export default OptionsModal;
+export {};
--- src/pages/HomePage/style.ts
+++ src/pages/HomePage/style.ts
@@ -7,10 +7,21 @@ export const MainHomePageContainer = styled.main`
   min-height: 100vh;
 `;
 
-export const HomePageContainer = styled.section`
+export const HomePageContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  row-gap: 20px;
   margin-top: 70px;
   width: 90%;
+`;
+
+export const HomePageSchoolsContainer = styled.section`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  .schools_container {
+    display: flex;
+    flex-direction: column;
+    row-gap: 20px;
+  }
 `;
